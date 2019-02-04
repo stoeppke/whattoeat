c8e21dfb035a613030b36b832c7f526c9890919e
stoeppke <stoeppke@gmail.com>
2019-02-04 17:04:12 +0100

    todo mark: move webapp to srv

--- docker-webapp.bash
+++ docker-webapp.bash
@@ -7,13 +7,13 @@ VIRTUAL_HOST_List_spaceSep=$(echo -n "www."$publicHostname)
 
 publicIpv4=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
-cd /tmp
+cd /tmp #TODO: move website stuff to /srv 
 
 # -> setting up .env file
     # -> comainion Host setup for this web-app
         sed -ie 's;VIRTUAL_HOST_frontend.*;VIRTUAL_HOST_frontend='$VIRTUAL_HOST_frontend';' .env
         sed -ie 's;VIRTUAL_HOST_backend.*;VIRTUAL_HOST_backend='$VIRTUAL_HOST_backend';' .env
-        sed -ie 's;VIRTUAL_HOST_backend.*;https://'$VIRTUAL_HOST_backend';' index.html
+        sed -ie 's;VIRTUAL_HOST_backend.*;https://'$VIRTUAL_HOST_backend'";' index.html
 
         sed -ie 's;your_email@yourdomain.com;stoeppke@gmail.com;g' .env
     # <- comanion END
